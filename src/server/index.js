fb1d602a2630fb8fe9587c2f42c5834db906a41d
Pramod <pramodsvidyarthi@gmail.com>
2017-11-11 12:19:23 +0530

    initial setup

---
+++ src/server/index.js
@@ -0,0 +1,3 @@
+import express from 'express';
+import React from 'React';
+import App from './shared/App';
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+import webpack from "webpack";
+import ExtractTextWebpackPlugin from "extract-text-webpack-plugin";
+import autoprefixer from "autoprefixer";
+
+const browserConfig = {
+  entry: "./src/browser/index.js",
+  ouput: {
+    path: __dirname,
+    filename: "./public/bundle.js"
+  },
+  devtool: "cheap-module-source-map",
+  modules: {
+    rules: [
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "public/media/[name].[ext]",
+              publicPath: url => url.replace("/public/", "")
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextWebpackPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: { importLoaders: 1 }
+            },
+            {
+              loader: "postcss-loader",
+              options: { plugins: [autoprefixer()] }
+            }
+          ]
+        })
+      },
+      {
+        test: /js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: { presets: ["react-app"] }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextWebpackPlugin({
+      filename: "public/css/[name].css"
+    })
+  ]
+};
+
+const serverConfig = {
+  entry: "./src/server/index.js",
+  target: "node",
+  output: {
+    path: __dirname,
+    filename: "server.js",
+    libraryTarget: "commonjs2"
+  },
+  modules: {
+    rules: [
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "public/media/[name].[ext]",
+              publicPath: url => url.replace("/public/", ""),
+              emit: false
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader/locals"
+          }
+        ]
+      },
+      {
+        test: /js$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: { presets: ["react-app"] }
+      }
+    ]
+  }
+};
+
+export default [browserConfig, serverConfig];
